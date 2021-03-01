18c2cb6b855f196249add6b41480222ce6516215
Saadoun Al-dhirat <saadoundhirat93@gmail.com>
2021-03-01 14:20:28 +0200

    update my lab solution based on emad notes

--- Lab: 01b - Getting Up and Running/code.js
+++ Lab: 01b - Getting Up and Running/code.js
@@ -1,9 +1,13 @@
 let userconfirm = confirm("if you want to continue please enter yes else no");
 if (userconfirm == true) {
   let firtsname = prompt("hello user please enter your first name");
+  console.log("answer:" + firtsname);
   let lastname = prompt("hello user please enter your last name");
+  console.log("answer:" + lastname);
   let age = prompt("hello user please enter age");
+  console.log("answer:" + age);
   let major = prompt("hello user please enter major");
+  console.log("answer:" + major);
   let country = prompt(
     "HELLO" +
       firtsname +
@@ -13,6 +17,7 @@ if (userconfirm == true) {
       "CLOSE TO MY MINE" +
       "\nFOR LAST PLEASE ENTER WHERE ARE YOU FROM"
   );
+  console.log("answer:" + country);
 
   alert(
     "FIRST AND LAST NAME:\n" +
